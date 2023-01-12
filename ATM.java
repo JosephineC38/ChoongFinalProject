a38be24c73ef8d0e4a9295e1810d12d71240ce67
josephinec38 <josephinec38@nycstudents.net>
2023-01-12 11:55:10 -0500

    Choice 5 kinda of done

--- ATM.java
+++ ATM.java
@@ -4,26 +4,39 @@ public class ATM {
     Customer customer = new Customer();
 
     private String decision;
-    public ATM () {}
+    private int id;
+
+    private int receipt;
+    public ATM () {
+        id = 10000;
+        receipt = 0;
+    }
 
     public void start() {
         //Asks for customer's name
-        System.out.println("Welcome to the bank!");
+        System.out.println("Welcome to the New York Bank!");
         System.out.print("What is your name: ");
         String name = scan.nextLine();
         customer.setName(name);
-        System.out.println(name + ", what a wonderful name");
+        System.out.println("Hello, " + name + ". I hope you enjoy your visit!");
 
         //Asks for customer's PIN
+        System.out.println("Your PIN has to be 4 numbers");
         System.out.print("What is your PIN: ");
         int pin = scan.nextInt();
+        String strPin = Integer.toString(pin);
+        while (strPin.length() != 4) {
+            System.out.println("Your PIN isn't 4 numbers. Please try again.");
+            System.out.print("What is your PIN: ");
+            pin = scan.nextInt();
+            strPin = Integer.toString(pin);
+        }
         customer.setPin(pin);
+        System.out.println("Thank you for creating an account");
         menu();
     }
 
     public void menu() {
-
-        System.out.println("Welcome to the New York Bank!");
         System.out.print("Enter your PIN: ");
         int userPin = scan.nextInt();
         while (userPin != customer.getPin()) {
@@ -54,7 +67,7 @@ public class ATM {
                     quit = true;
                     break;
                 case "5":
-                    System.out.println("5");
+                    changePin();
                     quit = true;
                     break;
                 case "6":
@@ -65,9 +78,49 @@ public class ATM {
                     System.out.println("That is an invalid choice. Please try again.");
                     System.out.print("Pick an option: ");
                     decision = scan.nextLine();
-                    break;
             }
         } while (!quit);
+    }
+
+    private void changePin() {
+        System.out.println("Your current PIN is " + customer.getPin() + ".");
+        System.out.print("Enter your new PIN: ");
+        int newPin = scan.nextInt();
+        customer.setPin(newPin);
+        receipt = 5;
+        printReceipt();
+    }
+
+    private void printReceipt() {
+        id++;
+        System.out.println("----------");
+        System.out.println("Receipt");
+        System.out.println("----");
+        System.out.println("Account Transaction: " + id);
+        if(receipt == 5) {
+            System.out.println("  - Changed PIN. The new PIN is " + customer.getPin());
+        }
+        System.out.println("----------\n");
+
+        System.out.print("Would you like to do anything else: ");
+        String choice = scan.nextLine();
+        choice = choice.toLowerCase();
+        while (!choice.equals("no") && !choice.equals("yes")) {
+                System.out.println("That isn't an option. Please try again.");
+                System.out.print("Would you like to do anything else: ");
+                choice = scan.nextLine();
+                choice = choice.toLowerCase();
+
+        }
+        if (choice.equals("yes")) {
+            System.out.println("Returning to menu");
+            menu();
+        } else if (choice.equals("no")) {
+            System.out.println("Thank you for being a customer at New York Bank. Come again!");
+        }
+
+
+
     }
 
 
